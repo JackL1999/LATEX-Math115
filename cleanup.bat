e627e4369ce01b0a97ce6f75169e06782805564a
Jack Lingbeck <j.lingbeck99@gmail.com>
2025-08-28 20:14:07 -0700

    Linux/Win Cleanup Tweaks

--- cleanup.bat
+++ cleanup.bat
@@ -6,5 +6,5 @@ rem 1. Open the VS Code Command Palette (Ctrl+Shift+P).
 rem 2. Type and select "Tasks: Run Task".
 rem 3. Choose "Remove All LaTeX Temporary Files" from the list.
 rem
-del /s *.aux *.bbl *.bcf *.blg *.dvi *.fls *.fdb_latexmk *.log *.lof *.lot *.nav *.out *.snm *.toc *.gz
+del /s *.aux *.bbl *.bcf *.blg *.dvi *.fls *.fdb_latexmk *.log *.lof *.lot *.nav *.out *.snm *.toc *.gz *.fdb_latexmk
 echo LaTeX auxiliary files have been cleaned up.
